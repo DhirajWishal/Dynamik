2f0337db0f230bc4779d388d4f81a2314ce2a61e
Dhiraj Wishal <wishaldhiraj@gmail.com>
2020-05-24 13:53:03 +0530

    Removed premake5 from gitignore.

---
+++ premake5.lua
@@ -0,0 +1,65 @@
+workspace "Dynamik"
+	architecture "x64"
+
+	platforms { "Windows", "Unix", "Mac" }
+
+	configurations {
+		"Debug Engine",
+		"Debug Studio",
+		"Release Engine",
+		"Release Studio",
+		"Distribution Engine",
+		"Distribution Studio"
+	}
+
+	filter "platforms:Windows"
+		system "windows"
+
+	filter "platforms:Unix"
+		system "linux"
+
+	filter "platforms:Mac"
+		system "macosx"
+
+outputDir = "$(Configuration)-$(Platform)"
+solutionDir = "$(SolutionDir)"
+configurationMacro = "$(Configuration)"
+platformMacro = "$(Platform)"
+projectNameMacro = "$(ProjectName)"
+
+engineSourcePath = "$(SolutionDir)Engine/Source/"
+engineOutputDir = "$(SolutionDir)Builds/Engine/"
+engineOutputBinaryDir = "Binaries/"
+engineOutputIntermediateDir = "Intermediate/"
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "$(SolutionDir)Dependencies/Libraries/External/glfw/include"
+IncludeDir["GLEW"] = "$(SolutionDir)Dependencies/GLEW/include"
+IncludeDir["glm"] = "$(SolutionDir)Dependencies/Libraries/External/glm"
+IncludeDir["stb"] = "$(SolutionDir)Dependencies/Libraries/External/stb"
+IncludeDir["tol"] = "$(SolutionDir)Dependencies/Libraries/External/tinyobjloader"
+IncludeDir["gli"] = "$(SolutionDir)Dependencies/Libraries/External/gli/gli"
+--Include the Vulkan SDK Include path here
+IncludeDir["Vulkan"] = "$(SolutionDir)Dependencies/Libraries/External/Vulkan/include"
+IncludeDir["boost"] = "E:/Programming/Modules and Runtime/boost_1_70_0"
+IncludeDir["jpeg"] = "$(SolutionDir)Dependencies/Libraries/Local/gil/jpeg-6b"
+IncludeDir["irrKlang"] = "$(SolutionDir)Dependencies/Libraries/Local/irrKlang/include"
+IncludeDir["assimp"] = "$(SolutionDir)Dependencies/Libraries/Local/assimp"
+
+IncludeLib = {}
+--Include the GLFW library path here
+IncludeLib["GLFW"] = "$(SolutionDir)Dependencies/Libraries/Binaries/GLFW/lib-vc2019"
+IncludeLib["GLEW"] = "$(SolutionDir)Dependencies/Libraries/Binaries/GLEW/Release/x64"
+--Include the Vulkan SDK path here
+IncludeLib["Vulkan"] = "$(SolutionDir)Dependencies/Libraries/Binaries/Vulkan"
+IncludeLib["DirectX"] = "$(SolutionDir)Dependencies/Libraries/Binaries/DirectX12"
+IncludeLib["irrKlang"] = "$(SolutionDir)Dependencies/Libraries/Binaries/irrKlang"
+IncludeLib["boostD"] = "E:/Programming/Modules and Runtime/boost_1_70_0/bin/Dependencies/iostreams/build/msvc-14.2/debug/address-model-64/link-static/threading-multi"
+IncludeLib["boostR"] = "E:/Programming/Modules and Runtime/boost_1_70_0/bin/Dependencies/iostreams/build/msvc-14.2/release/address-model-64/link-static/threading-multi"
+IncludeLib["Assimp"] = "$(SolutionDir)Dependencies/Libraries/Binaries/Assimp"
+
+group "Engine"	-- Add the Engine to a virtual folder
+include "Engine/Source/Dynamik.lua"
+include "Engine/Application/Application.lua"
+
+group ""
